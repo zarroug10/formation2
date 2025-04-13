d3b9b5cc69c3bc50675a58546d2943910e7e6580
Abdelhafidh ZARROUG <Azarroug@upper-link.com>
2025-04-13 17:28:42 +0100

    test2

--- dating_app/API/obj/Release/net9.0/API.AssemblyInfo.cs
+++ dating_app/API/obj/Release/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7209440c305a394da17229f6fde23524dfb0559e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
